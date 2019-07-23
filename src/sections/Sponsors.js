87aef0c01a505038a128b70741d3e8f259287086
Guilherme Bayer <guiilherme.bayer@gmail.com>
2019-07-23 13:30:11 -0300

    Adicionando FrontendBR como apoiador

--- src/sections/Sponsors.js
+++ src/sections/Sponsors.js
@@ -4,6 +4,7 @@ import { theme } from 'styled-tools'
 
 import veltec from '../images/veltec.png'
 import devparana from '../images/devparana.png'
+import frontendbr from '../images/frontendbr.png'
 
 const Container = styled.div`
   width: 100%;
@@ -34,6 +35,14 @@ const Container = styled.div`
     margin-top: 50px;
     margin-bottom: 30px;
   }
+
+  .partners {
+    width: 500px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
 `
 
 export default function Sponsors() {
@@ -51,13 +60,24 @@ export default function Sponsors() {
       </a>
 
       <h3>Apoiadores</h3>
-      <a
-        href="https://devparana.org/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={devparana} alt="Devparaná" />
-      </a>
+
+      <div className="partners">
+        <a
+          href="https://devparana.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={devparana} alt="Devparaná" />
+        </a>
+
+        <a
+          href="https://frontendbr.com.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={frontendbr} alt="Front-end BR" />
+        </a>
+      </div>
     </Container>
   )
 }
